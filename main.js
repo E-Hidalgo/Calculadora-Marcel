fb0c67ead47b737f11bc9bce9133f2274b73259f
MarcelDurgante <marceldur@gmail.com>
2021-08-16 13:07:25 +0200

    variables declared and started adding event listeners and functions

--- main.js
+++ main.js
@@ -15,41 +15,51 @@ let clear = document.getElementById("clear");
 let keys = Array.from(key);
 let operators = Array.from(operator);
 let numbers = Array.from(number);
+console.log(operators)
 
 // -------- EVENT LISTENER CHANGING CLASSES --------
 
 toggleBtn.addEventListener("click", () => {
-  mainContainer.classList.toggle("main-container--light");
-  miniScreen.classList.toggle("mini-screen--light");
-  screen.classList.toggle("screen--light");
-  keyboardGrid.classList.toggle("keyboard-grid--light");
-  keys.forEach((element) => {
-    element.classList.toggle("key--light");
-  });
-  operators.forEach((element) => {
-    element.classList.toggle("operator--light");
-  });
-  numbers.forEach((element) => {
-    element.classList.toggle("number--light");
-  });
-  equal.classList.toggle("equal--light");
-  label.classList.toggle("label--light");
+    mainContainer.classList.toggle("main-container--light");
+    miniScreen.classList.toggle("mini-screen--light");
+    screen.classList.toggle("screen--light");
+    keyboardGrid.classList.toggle("keyboard-grid--light");
+    keys.forEach((element) => {
+        element.classList.toggle("key--light");
+    });
+    operators.forEach((element) => {
+        element.classList.toggle("operator--light");
+    });
+    numbers.forEach((element) => {
+        element.classList.toggle("number--light");
+    });
+    equal.classList.toggle("equal--light");
+    label.classList.toggle("label--light");
 });
 
-// -------- CLEAR BUTTON --------
-clear.addEventListener("click", () => {
-  screen.innerHTML = "";
-  miniScreen.innerHTML = "";
+// -------- PRINTING OPERATORS --------
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (e) => {
+        screen.textContent += e.target.value;
+        // -------- CLEAR BUTTON --------
+        if (operator.value == "C") {
+            screen.innerHTML = "";
+            miniScreen.innerHTML = "";
+        }
+
+    });
 });
 
 // -------- PRINTING NUMBERS --------
 
-keys.forEach((key) => {
-  key.addEventListener("click", (e) => {
-    screen.textContent += e.target.textContent;
-
-    if (key.textContent == "C") {
-      screen.innerHTML = "";
-    }
-  });
-});
+numbers.forEach(number => {
+            number.addEventListener('click', (e) => {
+                screen.textContent += e.target.value;
+                if (number.value == "+" ||
+                    number.value == "-" ||
+                    number.value == "*" ||
+                    number.value == "/" ||
+                    number.value == "%" ||
+                    number.value == "+-") {}
+            });
