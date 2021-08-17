eaf5ef40dfa7602f49e2dfb3074f0f957178d6cc
Enrique Hidalgo <koko.freelance@gmail.com>
2021-08-17 10:29:44 +0100

    Operators almost working, and shoeing kind of result

--- main.js
+++ main.js
@@ -19,52 +19,62 @@ let numbers = Array.from(number);
 // -------- EVENT LISTENER CHANGING CLASSES --------
 
 toggleBtn.addEventListener("click", () => {
-    mainContainer.classList.toggle("main-container--light");
-    miniScreen.classList.toggle("mini-screen--light");
-    screen.classList.toggle("screen--light");
-    keyboardGrid.classList.toggle("keyboard-grid--light");
-    keys.forEach((element) => {
-        element.classList.toggle("key--light");
-    });
-    operators.forEach((element) => {
-        element.classList.toggle("operator--light");
-    });
-    numbers.forEach((element) => {
-        element.classList.toggle("number--light");
-    });
-    equal.classList.toggle("equal--light");
-    label.classList.toggle("label--light");
+  mainContainer.classList.toggle("main-container--light");
+  miniScreen.classList.toggle("mini-screen--light");
+  screen.classList.toggle("screen--light");
+  keyboardGrid.classList.toggle("keyboard-grid--light");
+  keys.forEach((element) => {
+    element.classList.toggle("key--light");
+  });
+  operators.forEach((element) => {
+    element.classList.toggle("operator--light");
+  });
+  numbers.forEach((element) => {
+    element.classList.toggle("number--light");
+  });
+  equal.classList.toggle("equal--light");
+  label.classList.toggle("label--light");
 });
 
 // -------- PRINTING OPERATORS --------
 
 operators.forEach((operator) => {
-    operator.addEventListener("click", (e) => {
-        screen.textContent += e.target.innerHTML; // it also works with e.target.value, it will return an array of the buttons too
-        // -------- CLEAR BUTTON --------
-        if (operator.textContent == "C") {
-            screen.textContent = 0;
-            miniScreen.textContent = 0;
-        } else if (screen.textContent.slice(-1) === "+" ||
-            screen.textContent.slice(-1) === "-" ||
-            screen.textContent.slice(-1) === "*" ||
-            screen.textContent.slice(-1) === "/" ||
-            screen.textContent.slice(-1) === "%" ||
-            screen.textContent.slice(-1) === "+-") {} else {
-            screen.textContent += this.value
-        }
-    });
-});
+  operator.addEventListener("click", (e) => {
+    // -------- CLEAR BUTTON --------
+    if (operator.innerHTML == "C") {
+      screen.innerHTML = 0;
+      miniScreen.innerHTML = 0;
 
+      // -------- SHOW ONLY ONE OPERATOR --------
+    } else if (
+      screen.innerHTML.slice(-1) == "+" ||
+      screen.innerHTML.slice(-1) == "-" ||
+      screen.innerHTML.slice(-1) == "x" ||
+      screen.innerHTML.slice(-1) == "/" ||
+      screen.innerHTML.slice(-1) == "%" ||
+      screen.innerHTML.slice(-1) == "+-"
+    ) {
+    } else {
+      screen.innerHTML += e.target.innerHTML; // it also works with e.target.value, it will return an array of the buttons too
+    }
+  });
+});
 
 // -------- PRINTING NUMBERS --------
 
-numbers.forEach(number => {
-    number.addEventListener('click', (e) => {
-        screen.textContent += e.target.value;
-        /*  if (!screen.innerHTML == "") {
-             screen.textContent = "";
-         } */
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (screen.textContent == 0) {
+      screen.textContent = e.target.value;
+    } else {
+      screen.textContent += e.target.value;
+    }
+  });
+});
 
-    });
+// -------- PRINT MINI-SCREEN AFTER EQUAL --------
+
+equal.addEventListener("click", () => {
+  miniScreen.textContent = screen.textContent;
+  screen.textContent = "RESULTADO";
 });
